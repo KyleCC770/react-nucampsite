6b2ab9a7c6d103d377fd50253c4af14a3ea51834
Kyle Collins <kyle.collins7737@gmail.com>
2020-12-10 13:13:31 -0500

    Added redux thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -18,6 +18,7 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -54,10 +55,8 @@ class CommentForm extends Component {
   }
 
   handleSubmit(values) {
-    console.log("Current state is: " + JSON.stringify(values));
-    alert("Current state is: " + JSON.stringify(values));
     this.toggleModal();
-
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
 
   render() {
@@ -136,7 +135,7 @@ class CommentForm extends Component {
 
 
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -156,7 +155,7 @@ function RenderComments({ comments }) {
             </div>
           );
         })}
-        <CommentForm/>
+        <CommentForm campsiteId={campsiteId}  addComment={addComment} />
       </div>
     );
   } else {
@@ -165,6 +164,26 @@ function RenderComments({ comments }) {
 }
 
 function CampsiteInfo(props) {
+  if(props.isLoading){
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading/>
+        </div>
+      </div>
+    );
+  }
+  if(props.errMess){
+    return (
+      <div className = "contianer">
+        <div className = "row">
+          <div className= "col">
+            <h4>{props.errMess}</h4>
+          </div>
+        </div>
+      </div>
+    )
+  }
   if (props.campsite) {
     return (
       <div>
@@ -183,7 +202,11 @@ function CampsiteInfo(props) {
           </div>
           <div className="row">
             <RenderCampsite campsite={props.campsite} />
-            <RenderComments comments={props.comments} />
+            <RenderComments
+              comments={props.comments} 
+              addComment={props.addComment}
+              campsiteId={props.campsite.id}
+            />
           </div>
         </div>
       </div>
